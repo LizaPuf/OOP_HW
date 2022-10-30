174c2f1b97ab32e492f9584be6756c22fbb137b7
Elizaveta Slonova <eparisgit config --global user.email eparis>
2022-10-30 14:35:09 +0300

    Task 2: remove rate_hw() method from Mentor class

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
 
 class Lecturer(Mentor):
     pass
