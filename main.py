aa2e176a83bbfe1c32d01b175e7f97e5a6bee857
Elizaveta Slonova <eparisgit config --global user.email eparis>
2022-10-30 13:48:33 +0300

    Task 2: add rate_hw() method for Reviewer class

--- main.py
+++ main.py
@@ -31,6 +31,14 @@ class Reviewer(Mentor):
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\n'
 
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
